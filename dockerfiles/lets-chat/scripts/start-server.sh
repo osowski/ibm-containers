ba8b7349f5e586a0c6a2a2f81dfce6027cb79aee
Rick Osowski <osowski@us.ibm.com>
2015-08-26 17:29:16 -0400

    Working lets-chat container, version 1

--- dockerfiles/lets-chat/scripts/start-server.sh
+++ dockerfiles/lets-chat/scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Extract Mongo credentials from VCAP_SERVICES
-TEMP=`python extract-vcap.py mongolab 0 credentials.uri`
+TEMP=`python scripts/extract-vcap.py mongolab 0 credentials.uri`
 if [ "$?" = "0" ]; then
 	export LCB_DATABASE_URI=${TEMP}
 fi
