29f189dd90f8867ed7278984424901a35bd49cb1
Rick Osowski <rosowski@gmail.com>
2015-09-15 03:50:17 -0500

    Added recommended sleep statement to lets-chat server startup script

--- dockerfiles/lets-chat/scripts/start-server.sh
+++ dockerfiles/lets-chat/scripts/start-server.sh
@@ -6,5 +6,8 @@ if [ "$?" = "0" ]; then
 	export LCB_DATABASE_URI=${TEMP}
 fi
 
+# Inserting sleep delay per recommendation via https://www.ng.bluemix.net/docs/containers/container_single_ov.html#container_single_cli
+sleep 35
+
 #Run server
 npm start
